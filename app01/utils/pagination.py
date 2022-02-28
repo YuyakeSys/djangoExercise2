6bd34ea9d9e6f7181b5b45006cbb93433b8c2796
YuyakeSys <1030321861@qq.com>
2022-03-01 01:22:52 +0800

    3.0

--- app01/utils/pagination.py
+++ app01/utils/pagination.py
@@ -1,9 +1,13 @@
 # Pagination component
 class Pagination(object):
 
-    def __init__(self, request, page_param='page'):
+    def __init__(self, request, queryset, page_size=3, page_param='page'):
         page = request.GET.get(page_param, "1")
         if page.isdecimal():
             page = int(page)
         else:
             page = 1
+        self.page = page
+        self.page_size = page_size
+
+
